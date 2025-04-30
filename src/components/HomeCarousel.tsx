1e6a3f77df81bf307993aad9d4166ad44de88a39
Princenetlegacy <47367105+Princenetlegacy@users.noreply.github.com>
2025-04-30 03:43:57 +0100

    hhghjg

--- src/components/HomeCarousel.tsx
+++ src/components/HomeCarousel.tsx
@@ -98,7 +98,7 @@ const HomeCarousel = () => {
             <AspectRatio ratio={16/9} className="h-full">
               <video 
                 className="w-full h-full object-cover"
-                src="https://player.vimeo.com/external/371433846.sd.mp4?s=236da2f3c0fd273d2c6d9a064f3ae35579b2bbdf&profile_id=139&oauth2_token_id=57447761"
+                src=""
                 autoPlay
                 muted
                 loop
