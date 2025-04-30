c709684bd37938dbd78eda65fabe4155f8afcee0
Princenetlegacy <47367105+Princenetlegacy@users.noreply.github.com>
2025-04-30 04:18:52 +0100

    kkm

--- src/components/home/FeaturedProperties.tsx
+++ src/components/home/FeaturedProperties.tsx
@@ -10,7 +10,7 @@ const featuredProperties = [
     id: '1',
     title: 'Hampton Vile Estate',
     location: 'Itoikin, Epe, Lagos',
-    price: '₦3,250,000',
+    price: '₦3,500,000',
     imageUrl: 'https://images.unsplash.com/photo-1613977257363-707ba9348227?ixlib=rb-4.0.3&auto=format&fit=crop&w=800&q=80',
     sqm: 500,
     propertyType: 'Land',
