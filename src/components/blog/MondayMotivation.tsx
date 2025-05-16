2a111c926412a1cf09b85ba3f8d6f24bb326453e
Princenetlegacy <47367105+Princenetlegacy@users.noreply.github.com>
2025-05-16 22:47:01 +0100

    no link in moday mot

--- src/components/blog/MondayMotivation.tsx
+++ src/components/blog/MondayMotivation.tsx
@@ -104,6 +104,7 @@ const MondayMotivation = () => {
                   {post.excerpt}
                 </p>
                 
+                {/*
                 <Link 
                   to={`/blog/motivation/${post.id}`} 
                   className="text-estate-blue font-medium hover:underline inline-flex items-center"
@@ -112,7 +113,7 @@ const MondayMotivation = () => {
                   <svg xmlns="http://www.w3.org/2000/svg" className="h-4 w-4 ml-1" viewBox="0 0 20 20" fill="currentColor">
                     <path fillRule="evenodd" d="M12.293 5.293a1 1 0 011.414 0l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-2.293-2.293a1 1 0 010-1.414z" clipRule="evenodd" />
                   </svg>
-                </Link>
+                </Link>*/}
               </div>
             </div>
           ))}
