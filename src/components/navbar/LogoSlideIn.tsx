5061a114f43bd801e1c11d6f9824be1467fd498e
Emmanuel Etokakpan <142060682+pnet45@users.noreply.github.com>
2026-03-05 15:29:50 +0100

    LOG16-18

--- src/components/navbar/LogoSlideIn.tsx
+++ src/components/navbar/LogoSlideIn.tsx
@@ -19,7 +19,7 @@ const LogoSlideIn = () => {
       <img
         src="/lovable-uploads/PWANBridgefortLogo.png"
         alt="PWAN Bridgefort Logo"
-        className={`h-16 w-auto transition-all duration-1000 ease-out ${
+        className={`h-18 w-auto transition-all duration-1000 ease-out ${
           isVisible 
             ? 'transform translate-x-0 opacity-100' 
             : 'transform -translate-x-full opacity-0'
