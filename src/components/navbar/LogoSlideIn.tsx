9e49ed4d91fdc3367cef39beab08e56ee6432074
Emmanuel Etokakpan <142060682+pnet45@users.noreply.github.com>
2026-05-15 16:53:21 +0100

    h-10

--- src/components/navbar/LogoSlideIn.tsx
+++ src/components/navbar/LogoSlideIn.tsx
@@ -19,7 +19,7 @@ const LogoSlideIn = () => {
       <img
         src="/lovable-uploads/BridgefortHomesLogo.png"
         alt="Bridgefort Homes Development Ltd Logo"
-        className={`h-13 w-auto transition-all duration-2000 ease-out ${
+        className={`h-10 w-auto transition-all duration-2000 ease-out ${
           isVisible 
             ? 'transform translate-x-0 opacity-100' 
             : 'transform -translate-x-full opacity-0'
