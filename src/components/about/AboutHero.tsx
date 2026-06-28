7ce77bb0dc6669e5964bc3c064c8072590730c1b
Emmanuel Etokakpan <142060682+pnet45@users.noreply.github.com>
2026-06-28 18:20:52 +0100

    AboutHero
    card pass

--- src/components/about/AboutHero.tsx
+++ src/components/about/AboutHero.tsx
@@ -4,9 +4,9 @@ const AboutHero = () => {
       <div style={{
       backgroundImage: 'url(https://images.unsplash.com/photo-1497366754035-f200968a6e72?ixlib=rb-4.0.3&auto=format&fit=crop&w=1500&q=80)'
     }} className="h-[40vh] bg-cover bg-center bg-indigo-950">
-        <div className="absolute inset-0 bg-black bg-opacity-50 flex items-center my-0">
+        <div className="absolute inset-0 hero-overlay flex items-center my-0">
           <div className="container-custom text-white">
-            <h2 className="text-3xl md:text-5xl font-bold mb-4 my-[6px]">About Bridgefort Homes Development Ltd</h2>
+            <h2 className="text-3xl md:text-5xl font-bold mb-4 my-[6px] text-gradient">About Bridgefort Homes Development Ltd</h2>
             <p className="text-xl max-w-2xl my-0">Learn about our mission, vision, and the team behind Nigeria's leading real estate investment company.</p>
           </div>
         </div>
