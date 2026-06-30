156f604561be5e3c677327f5c0d06aeccb253039
Emmanuel Etokakpan <142060682+pnet45@users.noreply.github.com>
2026-06-30 05:46:13 +0100

    enh vis

--- src/components/buy2sell/Buy2SellHero.tsx
+++ src/components/buy2sell/Buy2SellHero.tsx
@@ -12,7 +12,7 @@ const Buy2SellHero: React.FC<Buy2SellHeroProps> = ({
         <div className="absolute inset-0 hero-overlay flex items-center">
           <div className="container-custom text-white">
             <h1 className="text-3xl font-bold mb-4 py-[33px] md:text-5xl px-[6px] mx-0 my-[43px] text-gradient">Buy and Resell Real Estate Trading Platform</h1>
-            <p className="text-xl max-w-3xl text-center">Secure, Profitable Real Estate Trading – Tailored Just for You. A revolutionary approach to real estate land banking with guaranteed returns.</p>
+            <p className="text-xl max-w-3xl text-center hero-text">Secure, Profitable Real Estate Trading – Tailored Just for You. A revolutionary approach to real estate land banking with guaranteed returns.</p>
           </div>
         </div>
       </div>
