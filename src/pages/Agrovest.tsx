664354be9ee1b9b814460ffe26fd2a5a59371902
pnet45 <emmanuel.e.etokakpan@gmail.com>
2026-08-24 13:07:40 +0100

    edit Agrovest

--- src/pages/Agrovest.tsx
+++ src/pages/Agrovest.tsx
@@ -434,7 +434,7 @@ const Agrovest: React.FC = () => {
               harvesting and marketing — while investors receive agreed profit shares.
             </p>
             <p className="text-xl font-extrabold tracking-wide">
-              You Invest. <span className="text-amber-300">We Farm.</span> <span className="text-amber-900">You Earn.</span>
+              You Invest. <span className="text-amber-300 tracking-wide">We Farm.</span> <span className="text-green-900 font-extrabold tracking-wide">You Earn.</span>
             </p>
           </Glass>
         </div>
